811b50206a198af4f1d481290eb39e1b8a49398a
gitx5622 <ggitau5622@gmail.com>
2020-12-13 10:12:02 +0300

    added authentication

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
 import { Row, Col } from 'antd';
+import { Button } from 'shards-react';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from "react-redux";
+import { SignOut } from '../store/auth/actions/authActions';
 
 const NavBar = () => {
+    const currentState = useSelector((state) => state);
+    
+    const { isAuthenticated } = currentState.Auth;
+  
+    const dispatch = useDispatch();
+  
+    const logoutUser  = () => dispatch(SignOut());
+  
+    const logout = (e) => {
+        e.preventDefault();
+        logoutUser()
+    };
+      
+    const signInLinks = (
+        <>
+            <li><a href="/login"><Button size="sm" outline theme="info">Login</Button></a></li>
+            <li><a href="/register"><Button size="sm" theme="info">Sign Up</Button></a></li>
+        </>
+      )
+      const signOutLinks = (
+            <li><a href="/login"><Button size="sm" onClick={logout} outline theme="info">Logout</Button></a></li>
+      )
     return ( 
         <div className="main-navbar fixed-top">
             <div className="container">
@@ -12,8 +37,7 @@ const NavBar = () => {
                 </Col>
                 <Col sm={12} xs={12}>
                 <ul className="auth-links">
-                    <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/register">Sign Up</Link></li>
+                { isAuthenticated ? signOutLinks: signInLinks }
                 </ul>
                 </Col>
             </Row>
